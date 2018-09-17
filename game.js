e8156049c515572ffed27b2b3803d1d48ff09a70
xiao8xiao1 <xiao8xiao_1@qq.com>
2018-09-17 22:29:19 +0800

    c

--- game.js
+++ game.js
@@ -145,19 +145,19 @@ function passDirLevel(){
 //html
 var c = new function () {
   this.gravity = 20
-  this.cameraY = 15
-  this.cameraZ = 30
+  this.cameraY = 17
+  this.cameraZ = 38
   this.LookatY = 15
   this.LookatZ = 0
   this.zoom = 1
-  this.disdanceHalf = 15
+  this.disdanceHalf = 20
   this.forceFactor = 0.1
   this.groundY = 9
   this.roomWidth = 30
   this.deskHight = 10
   this.ballRad = 1
-  this.canRad = 1.2
-  this.canHeight = 4
+  this.canRad = 1.4
+  this.canHeight = 5
   this.massBall = 5
   this.massCan = 10
   this.shootHeight = 4
@@ -342,7 +342,7 @@ function flyingCheck(){
       if (Math.abs(flyingBall.linearVelocity.z) < velocitySqLimit)
           return;
 
-      if (flyingBall.position.z < flyingBall.changeStepAtPosZ)
+      if (flyingBall.position.z < flyingBall.changeStepAtPosZ && flyingBall.position.z > -c.disdanceHalf)
       {
           if (flyingBall.newTimeStep < minTimeStep)
               minTimeStep = flyingBall.newTimeStep;
